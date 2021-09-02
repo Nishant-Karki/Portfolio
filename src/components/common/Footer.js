a9d5b9a59bd1a6ad95ec6452d386f437ed3593a5
Nishant Karki <karkinishant14@gmail.com>
2021-09-02 10:32:01 +0545

    Final Page

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -4,6 +4,7 @@ import "../sass/footer.scss";
 import { FaFacebookF, FaLinkedinIn, FaTwitter } from "react-icons/fa";
 import { VscGithub } from "react-icons/vsc";
 import { AiFillInstagram } from "react-icons/ai";
+import moment from "moment";
 
 function Footer() {
   const { Title } = Typography;
@@ -18,8 +19,8 @@ function Footer() {
     >
       <Content style={{ padding: "15% 15%" }}>
         <Row>
-          <Col md={12} xs={24}>
-            <div style={{ display: "flex" }}>
+          <Col md={12} xs={24} style={{ marginBottom: "4rem" }}>
+            <div style={{ display: "flex", justifyContent: "center" }}>
               <FaFacebookF className="social-icon" />
               <FaTwitter className="social-icon" />
               <AiFillInstagram className="social-icon" />
@@ -27,16 +28,20 @@ function Footer() {
               <VscGithub className="social-icon" />
             </div>
           </Col>
-          <Col md={12} xs={24}>
+          <Col md={12} xs={24} style={{ marginBottom: "3rem" }}>
             <Title level={3} style={{ color: "whitesmoke" }}>
               GET IN TOUCH
             </Title>
             <div className="contact-container">
               <div className="contact-details">{user.email}</div>
               <div className="contact-details">{user.phone}</div>
+              <div className="contact-details">{user.address}</div>
             </div>
           </Col>
         </Row>
+        <div style={{ textAlign: "center" }}>
+          &copy; {moment().format("YYYY")} All right reserved.
+        </div>
       </Content>
     </div>
   );
